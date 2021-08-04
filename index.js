8caa6181fb81062e26d0580306a990b96fb5ad35
mbaire <mbairemunge@gmail.com>
2021-08-04 03:09:42 +0300

    Modified Js file

--- index.js
+++ index.js
@@ -24,4 +24,32 @@ $(document).ready(function() {
       $("div#prod-content").toggle();
      $("div#product").toggle();
     });
-  
+    
+    $("#col-1").hover(function() {
+        $("#text1").fadeTo("slow",1);
+        $("#photo1").fadeTo("slow",0.4);
+        },function() {
+          $("#text1").fadeTo("slow",0);
+          $("#photo1").fadeTo("slow",1);
+        });
+      $("#col-2").hover(function() {
+        $("#text2").fadeTo("slow",1);
+        $("#photo2").fadeTo("slow",0.4);
+        },function() {
+          $("#text2").fadeTo("slow",0);
+          $("#photo2").fadeTo("slow",1);
+        });
+      $("#col-3").hover(function() {
+        $("#text3").fadeTo("slow",1);
+        $("#photo3").fadeTo("slow",0.4);;
+        },function() {
+          $("#text3").fadeTo("slow",0);
+          $("#photo3").fadeTo("slow",1);
+        });
+      $("#col-4").hover(function() {
+        $("#text4").fadeTo("slow",1);
+        $("#photo4").fadeTo("slow",0.4);
+        },function() {
+          $("#text4").fadeTo("slow",0);
+          $("#photo4").fadeTo("slow",1);
+        });  
